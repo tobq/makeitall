481d8ac1bd5b2351eba1d3faefb1f2363b41aba7
Tobi <tobi.akiny@gmail.com>
2018-12-18 16:03:37 +0000

    configured initial git repo

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Make It All</title>
+    <meta name="description" content="Make it all - Tech support web portal">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+<nav>
+    <img id="logo" src="/i/logo.png" style="height: 100px"/>
+    <a href="">
+        Home
+    </a><a href="">
+        Report Problem
+    </a><a href="">
+        Problems and Calls
+    </a><a href="">
+        Employees
+    </a><a href="">
+        Technologies
+    </a>
+
+    <a href="" id="log-out-button">Log Out</a>
+</nav>
+BING BANG BONG
+@section("content")
+@show
+<script src="js/app.js"></script>
+</body>
+
+</html>
