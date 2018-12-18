960c9bfaa3f5fc7b31cb6652bd26d4f3d4c96f40
Tobi <tobi.akiny@gmail.com>
2018-12-18 23:15:24 +0000

    finished designing base components

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
     <meta charset="utf-8">
@@ -7,28 +7,35 @@
     <meta name="description" content="Make it all - Tech support web portal">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="/css/app.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    @yield("head")
 </head>
 
 <body>
 <nav>
-    <img id="logo" src="/i/logo.png" style="height: 100px"/>
-    <a href="">
-        Home
-    </a><a href="">
-        Report Problem
-    </a><a href="">
-        Problems and Calls
-    </a><a href="">
-        Employees
-    </a><a href="">
-        Technologies
-    </a>
-
-    <a href="" id="log-out-button">Log Out</a>
+    <a href="/" id="logo"><span id="logo-icon"></span>MakeItAll</a>
+    <a href=""><i class="material-icons">home</i>Dashboard</a>
+    <a href=""><i class="material-icons">assignment</i>Problems</a>
+    <a href=""><i class="material-icons">list</i>Calls</a>
+    <a href=""><i class="material-icons">group</i>Employees</a>
+    <a href=""><i class="material-icons">business_center</i>Equipment</a>
 </nav>
-BING BANG BONG
-@section("content")
-@show
+<header>
+    <a href="/call" id="new-call-button">
+        <i class="material-icons">call</i>New Call
+    </a><span id="user-display-picture">
+        <i class="material-icons">person</i>
+    </span>
+    <div id="user-menu">
+        <a id="user-menu-name" href="">$Alice</a>
+        <a href="">Settings</a>
+        <a href="">Something</a>
+        <a href="">Log Out</a>
+    </div>
+</header>
+
+@yield("content")
+
 <script src="js/app.js"></script>
 </body>
 
